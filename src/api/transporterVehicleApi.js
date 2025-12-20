05bd83888753f46f57ec39dbe21f6cce280c3223
Ritik <ritikraisinghani5@gmail.com>
2025-12-20 17:43:01 +0530

    transporter registraton

--- src/api/transporterVehicleApi.js
+++ src/api/transporterVehicleApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://my-backend-1-qxc9.onrender.com/api/transporter-vehicles",
+  baseURL: "https://my-backend-1-qxc9.onrender.com/api/transporter-vehicle",
   withCredentials: true,
 });
 
