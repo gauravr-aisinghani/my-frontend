8e9e8c966262eaaf461d33d4c1a1af67dca4ca97
Ritik <ritikraisinghani5@gmail.com>
2025-12-21 21:37:46 +0530

    ap file enhancement

--- src/api/driverReportApi.js
+++ src/api/driverReportApi.js
@@ -1,7 +1,20 @@
 import axios from "axios";
 
-const API_URL = "/api/reports/drivers";
+const api = axios.create({
+  // baseURL: "http://localhost:8089/api/drivers",
+  baseURL: "https://my-backend-1-qxc9.onrender.com/api/reports/drivers",
+  withCredentials: true,
+});
+
+
+
 
 export const fetchDriverReports = (params) => {
   return axios.get(API_URL, { params });
 };
+
+
+
+
+
+
