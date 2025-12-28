8b308257737c5995798aedae8a17c08f28326ef5
Ritik <ritikraisinghani5@gmail.com>
2025-12-28 10:45:36 +0530

    vehicle api changes

--- src/api/transporterVehicleApi.js
+++ src/api/transporterVehicleApi.js
@@ -1,14 +1,13 @@
 import api from "./axiosInstance";
 
-
-// const api = axios.create({
-//   baseURL: `${api}/api/transporter-vehicle`,
-//   withCredentials: true,
-// });
+// ✅ relative path for this module
+const BASE_URL = "/api/transporter-vehicle";
 
 export const saveTransporterVehicle = async (data) => {
   try {
-    const res = await api.post("", data);
+    const res = await api.post(BASE_URL, data, {
+      withCredentials: true,
+    });
     return res.data;
   } catch (error) {
     console.error("Error saving transporter vehicle:", error);
